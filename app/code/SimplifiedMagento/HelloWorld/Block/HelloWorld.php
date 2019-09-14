836d561c77d1a1d32dbc6121982ea38a798f98ca
Ahmad Saeed <ahmad.saeed.goda@gmail.com>
2019-09-14 12:09:23 +0200

    Passing Data From Custom Block To Template

--- app/code/SimplifiedMagento/HelloWorld/Block/HelloWorld.php
+++ app/code/SimplifiedMagento/HelloWorld/Block/HelloWorld.php
@@ -3,22 +3,41 @@
 namespace SimplifiedMagento\HelloWorld\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class HelloWorld extends Template
 {
+    protected $product;
+
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        Template\Context $context,
+        array $data = []
+    )
+    {
+        $this->product = $productRepository;
+        parent::__construct($context, $data);
+    }
+
     public function getHelloWorld()
     {
         return 'This Comes From Custom Block';
     }
     
-    public function gendArrayToTemplate()
+    public function getArrayToTemplate()
     {
-        $array_to_return = [
+        $arrayToReturn = [
             'First_element',
             'Second_element',
             'Third_element'
         ];
 
-        return $array_to_return;
+        return $arrayToReturn;
+    }
+
+    public function getProductName()
+    {
+        $product = $this->product->get('24-MB01');
+        return $product->getName();
     }
 }
