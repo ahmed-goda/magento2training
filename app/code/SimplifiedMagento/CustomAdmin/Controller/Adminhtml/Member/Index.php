283e10e117c4c58521c21b0f4e6b788a11bca9cb
Ahmed Goda <ahmed.goda@bytology.co.uk>
2019-09-14 16:22:38 +0200

    68. Adding a New Menu in Admin

---
+++ app/code/SimplifiedMagento/CustomAdmin/Controller/Adminhtml/Member/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SimplifiedMagento\CustomAdmin\Controller\Adminhtml\Member;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    private $pageFactory;
+
+    public function __construct(PageFactory $pageFactory, Context $context)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+    
+}
